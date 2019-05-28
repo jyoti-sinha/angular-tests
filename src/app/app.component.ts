5baf1a0dec4d4e27f112d3de470d816d0cdaaa1c
jyoti-sinha <jyoti40sinha@gmail.com>
2019-05-28 15:47:57 +0530

    async instead of sync data in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  ad_data: Array<any> = [
-    {
-      title: 'Where does it come from?',
-      desc: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.'
-    },
-    {
-      title: 'Why do we use it?',
-      desc: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
-    }
-  ];
+export class AppComponent implements OnInit {
+  showAds:boolean = false;
+  ad_data: Array<any> = [];
+  constructor(private http: HttpClient){}
+
+  ngOnInit() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts', {
+      headers: {
+        'content-type' : 'application/json'
+      }
+    }).subscribe((res:any) => {
+      res.forEach((item, i) => {
+        this.ad_data.push({
+          title: item.title,
+          desc: item.body
+        });
+      });
+      this.showAds = true;
+    })
+  }
 
 }
